a066e9a46d7b34ff9eb2639f13528d0459233384
Adriana Capozzi <145504525+AdrianaCapozzi@users.noreply.github.com>
2024-10-30 14:45:13 -0300

    conferir se é isso mesmo

---
+++ js/landing-page.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <header>
+        <img src={logo} alt="Logo da Prefeitura de São Vicente" className="logo" />
+        <h1>Prefeitura de São Vicente</h1>
+        <h2>Conecta São Vicente - Portal da Zeladoria</h2>
+      </header>
+
+      <p className="description">
+        Aqui, em alguns cliques, você solicita o serviço para cuidar da nossa cidade, consulta o status e manda seu recado!
+      </p>
+
+      {/* botão p conhecer os serviços da prefeitura, acham q fica ok fazer isso ou escreve sobre cada serviço? */}
+      <p className="info-link">
+        <a
+          href="https://www.saovicente.sp.gov.br/categoria/301"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="service-button"
+        >
+          Clique aqui para conhecer os serviços que esta página pode conectar você à prefeitura
+        </a>
+      </p>
+
+      {/* botão de login */}
+      <div className="login-button-container">
+        <a href="/login" className="login-button">
+          Login
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
